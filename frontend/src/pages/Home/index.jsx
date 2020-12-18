0e3232281398f1bb38d8d2503994f8cda3d91d2b
Marcus Berenguel <marcus_berenguel@hotmail.com>
2020-12-18 14:03:55 -0300

    atualizando home

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import Animate from '../../components/Animate';
+import CardCourse from '../../components/CardCourse'
 
 import './styles.css';
 
 import separator from '../../assets/images/separator.svg';
 import yoda from '../../assets/images/characters/yoda.png'
+import courseReactRedux from '../../assets/images/courses/react-redux.jpg';
+import coursePHP from '../../assets/images/courses/php.jpg';
+import courseFlutter from '../../assets/images/courses/flutter.jpg';
 
 
 const Home = () => {
@@ -33,6 +37,42 @@ const Home = () => {
                             <img src={yoda} alt="Yoda" />
                         </section>
                     </main>
+                </div>
+            </div>
+            <div className="section-2">
+                <div className="container">
+                    <h2>3 cursos grátis para você começar!</h2>
+                    <p>Na Luke Code, você tem acesso à 3 cursos gratuitos, basta se cadastrar em nossa plataforma.</p>
+                    <section className="card-container">
+                        <CardCourse image={courseReactRedux} description="React e Redux">
+                            Desenvolvimento front-end <br /> React e Redux
+                        </CardCourse>
+                        <CardCourse image={coursePHP} description="React e Redux">
+                            PHP 7 - Trabalhando com PSRs
+                        </CardCourse>
+                        <CardCourse image={courseFlutter} description="React e Redux">
+                            Desenvolvimento iOS e Android <br /> com Flutter
+                        </CardCourse>
+                    </section>
+                    <button>INSCREVA-SE</button>
+                </div>
+            </div>
+            <div className="section-3">
+                <div className="container">
+                    <h2>O que oferecemos a você</h2>
+                    <section className="categories-container">
+                        
+                        <div className="category-card">
+                            <div className="category-card-img">
+
+                            </div>
+                            <div className="category-card-content">
+                                <h4>Texto</h4>
+                                <p>Texto</p>
+                            </div>
+                        </div>
+                    
+                    </section>
                 </div>
             </div>
         </>
